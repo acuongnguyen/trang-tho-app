bb70a20bfba13aba6c5cd9e909dd7b09b6928bef
nguyenmanhcuong <cuong.nm@tinasoft.com.vn>
2023-11-16 15:46:13 +0700

    Redux

--- src/components/AccountRoute.tsx
+++ src/components/AccountRoute.tsx
@@ -1,7 +1,22 @@
-import React from 'react'
-
-export const AccountRoute = () => {
+import React from 'react';
+import { Route, RouteProps } from 'react-router-dom';
+import { AccountState } from '../store/account/types';
+import { useSelector } from 'react-redux';
+import { AppState } from '../store';
+import { Admin } from '../pages/Admin/Admin';
+import { Login } from '../pages/Account';
+import { Redirect } from 'react-router';
+export const AccountRoute = ({ children, ...rest }: RouteProps): JSX.Element => {
+    const account: AccountState = useSelector((state: AppState) => state.account);
     return (
-        <div></div>
-    )
-}
+        <Route
+            {...rest}
+            render={() =>
+                account.token ? (
+                    <Redirect to={{ pathname: 'admin/home' }} />
+                ) : (
+                    <Login />
+                )}
+        ></Route>
+    );
+};
--- src/components/PrivateRoute.tsx
+++ src/components/PrivateRoute.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import { Route, RouteProps } from 'react-router-dom';
 import { Login } from '../pages/Account';
-
+import { AccountState } from '../store/account/types';
+import { useSelector } from 'react-redux';
+import { AppState } from '../store';
+import { Admin } from '../pages/Admin/Admin';
 export const PrivateRoute = ({ children, ...rest }: RouteProps): JSX.Element => {
+    const account: AccountState = useSelector((state: AppState) => state.account);
     return (
-        <Route {...rest} render={() => (true ? children : <Login />)} />
+        <Route {...rest} render={() => (account.token ? children : <Login />)} />
     );
 };
